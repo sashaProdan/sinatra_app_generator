b6d38b017855f8f9d53112716af1b582b55a41ec
Alexandra Prodan <prodan.sasha@gmail.com>
2016-08-31 19:09:26 -0500

    Add method for creating files in root dir/ Add method for creating subfolders in root dir

--- generator.rb
+++ generator.rb
@@ -3,19 +3,27 @@ require "fileutils"
 
 
 class Directory
-
-DATA = Psych.load_file("file_content.yaml")
+  DATA = Psych.load_file("file_content.yaml")
 
   def initialize(name)
-    @new_app = FileUtils.mkdir(name)
+    @app_name = FileUtils::mkdir(name)
+  end
+
+  def generate
+    FileUtils::cd(app_name.first)
+
+    create_root_dir_files
+    create_sub_folders
   end
 
   def create_root_dir_files
-  
+    DATA['root_dir_files'].each do |file| 
+      file.each { |file, _| FileUtils::touch(file) }
+    end
   end
 
   def create_sub_folders
-
+    DATA['sub_dirs'].keys.each { |folder| FileUtils::mkdir(folder) }
   end
 
   def generate_root_dir_files_content
@@ -23,18 +31,14 @@ DATA = Psych.load_file("file_content.yaml")
   end
 
   def generate_sub_folders_content
-    
+
   end
 
   private
 
-    attr_accessor :new_app
-
+    attr_accessor :app_name
 end
 
+Directory.new('').generate
 
-
-
-
-Directory.new('todos')
 
